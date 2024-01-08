e5eb4c83a4d6661581276926bb6d444fdf8f5959
imCyberMohsin <mohsinansari7033@gmail.com>
2024-01-09 04:38:17 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Instagram Clone
+
+login & register page
+feed page
+profile page
+picture
+story feature
+edit details
+share to story
+search accounts
---
+++ routes/index.js
@@ -0,0 +1,95 @@
+var express = require('express');
+var router = express.Router();
+const userModel = require('./users');    // User Model
+const passport = require('passport');
+const localStrategy = require('passport-local')
+const upload = require('./multer')  // Multer script
+
+passport.use(new localStrategy(userModel.authenticate()));
+
+router.get('/', function (req, res) {
+  res.render('index', { footer: false });
+});
+
+router.get('/login', function (req, res) {
+  res.render('login', { footer: false });
+});
+
+router.get('/feed', isLoggedIn, function (req, res) {
+  res.render('feed', { footer: true });
+});
+
+// Profile 
+router.get('/profile', isLoggedIn, async function (req, res) {
+  const user = await userModel.findOne({ username: req.session.passport.user })
+  res.render('profile', { footer: true, user });
+});
+
+router.get('/search', isLoggedIn, function (req, res) {
+  res.render('search', { footer: true, user });
+});
+
+// edit profile 
+router.get('/edit', isLoggedIn, async function (req, res) {
+  const user = await userModel.findOne({ username : req.session.passport.user })
+  res.render('edit', { footer: true, user });
+});
+
+router.get('/upload', isLoggedIn, function (req, res) {
+  res.render('upload', { footer: true });
+});
+
+// Register Route
+router.post('/register', (req, res, next) => {
+  const userData = new userModel({
+    username: req.body.username,
+    name: req.body.name,
+    email: req.body.email,
+  })
+
+  userModel.register(userData, req.body.password)
+    .then(function () {
+      passport.authenticate('local')(req, res, function () {
+        res.redirect('/login');
+      })
+    })
+})
+
+// Login 
+router.post('/login', passport.authenticate('local', {
+  successRedirect: "/profile",
+  failureRedirect: '/login'
+}), function (req, res) {
+});
+
+// Logout
+router.get('/logout', function (req, res, next) {
+  req.logout(function (err) {
+    if (err) { return next(err) };
+    res.redirect('/login');
+  })
+});
+
+// Edit-update profile
+router.post('/update', upload.single('image'), async (req, res, next) => {
+  const user = await userModel.findOneAndUpdate(
+    { username: req.session.passport.user },
+    { username: req.body.username, name: req.body.name, bio: req.body.bio },
+    { new: true }
+  );
+
+  if(req.file){
+    user.profileImage = req.file.filename;
+  }
+
+  await user.save();
+  res.redirect('/profile');
+})
+
+// isLoggedIn
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect('/login');
+}
+
+module.exports = router;
