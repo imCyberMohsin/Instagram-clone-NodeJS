ec57bc5b4f99792b5d93c10c4eaf011bd33efb5c
imCyberMohsin <mohsinansari7033@gmail.com>
2024-01-10 04:56:41 +0530

    post creation, like feature added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const userModel = require('./users');    // User Model
+const postModel = require('./posts');    // User Model
 const passport = require('passport');
 const localStrategy = require('passport-local')
 const upload = require('./multer')  // Multer script
@@ -15,28 +16,80 @@ router.get('/login', function (req, res) {
   res.render('login', { footer: false });
 });
 
-router.get('/feed', isLoggedIn, function (req, res) {
-  res.render('feed', { footer: true });
+router.get('/feed', isLoggedIn, async function (req, res) {
+  const user = await userModel.findOne({ username: req.session.passport.user }).populate('posts')
+  const posts = await postModel.find().populate('user')
+  res.render('feed', { footer: true, posts, user });
 });
 
 // Profile 
 router.get('/profile', isLoggedIn, async function (req, res) {
-  const user = await userModel.findOne({ username: req.session.passport.user })
+  const user = await userModel.findOne({ username: req.session.passport.user }).populate('posts')
   res.render('profile', { footer: true, user });
 });
 
-router.get('/search', isLoggedIn, function (req, res) {
+router.get('/search', isLoggedIn, async function (req, res) {
+  const user = await userModel.find({ username: req.user })
   res.render('search', { footer: true, user });
 });
 
+router.get('/like/post/:id', isLoggedIn, async function (req, res) {
+  try {
+    const user = await userModel.findOne({ username: req.session.passport.user });
+    const post = await postModel.findById(req.params.id);
+
+    // Check if the user has already liked the post
+    const likedIndex = post.likes.indexOf(user._id);
+
+    if (likedIndex === -1) {
+      // If not liked, add the user to the likes array
+      post.likes.push(user._id);
+    } else {
+      // If already liked, remove the user from the likes array
+      post.likes.splice(likedIndex, 1);
+    }
+
+    // Save the updated post
+    await post.save();
+
+    // Redirect back to the post or the feed page
+    const referer = req.headers.referer || '/feed';
+    res.redirect(referer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
 // edit profile 
 router.get('/edit', isLoggedIn, async function (req, res) {
-  const user = await userModel.findOne({ username : req.session.passport.user })
+  const user = await userModel.findOne({ username: req.session.passport.user })
   res.render('edit', { footer: true, user });
 });
 
+// Upload Post 
 router.get('/upload', isLoggedIn, function (req, res) {
   res.render('upload', { footer: true });
+});
+
+router.post('/upload', upload.single('image'), isLoggedIn, async function (req, res) {
+  const user = await userModel.findOne({ username: req.session.passport.user })
+  const post = await postModel.create({
+    picture: req.file.filename,
+    user: user._id,
+    caption: req.body.caption,
+  })
+
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect('/feed');
+});
+
+router.get('/username/:username', isLoggedIn, async function (req, res) {
+  const regex = new RegExp(`^${req.params.username}`, 'i');
+  const users = await userModel.find({ username: regex })
+  res.json(users);
 });
 
 // Register Route
@@ -70,7 +123,7 @@ router.get('/logout', function (req, res, next) {
   })
 });
 
-// Edit-update profile
+// Edit-update profile details
 router.post('/update', upload.single('image'), async (req, res, next) => {
   const user = await userModel.findOneAndUpdate(
     { username: req.session.passport.user },
@@ -78,7 +131,7 @@ router.post('/update', upload.single('image'), async (req, res, next) => {
     { new: true }
   );
 
-  if(req.file){
+  if (req.file) {
     user.profileImage = req.file.filename;
   }
 
---
+++ routes/posts.js
@@ -0,0 +1,24 @@
+// Post Schema
+// User Schema
+const mongoose = require('mongoose')
+
+const postSchema = mongoose.Schema({
+    picture: String,
+    user: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "user",
+    },
+    caption: String,
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user',
+        }
+    ]
+})
+
+module.exports = mongoose.model('post', postSchema); 
