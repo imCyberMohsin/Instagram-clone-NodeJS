1f06403b345463563c2ef130846d0bc1357aa658
imCyberMohsin <mohsinansari7033@gmail.com>
2024-01-26 11:09:14 +0530

    production code changes

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -7,13 +6,18 @@ var logger = require('morgan');
 const expressSession = require('express-session')
 const passport = require('passport')
 
+//? Routes Imported From /routes 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+//? Middlewares 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static('public'))
 app.set('view engine', 'ejs');
 
 //? Persistance login session - using express session 
@@ -27,31 +31,11 @@ app.use(passport.session());
 passport.serializeUser(usersRouter.serializeUser());
 passport.deserializeUser(usersRouter.deserializeUser());
 
-
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+//? Using Routers 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server Running on Port ${port}`);
+})
